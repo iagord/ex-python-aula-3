5c66121ca89dd52cfcb3da4e44369f8b57bccdec
iagord <rosadiasiago@gmail.com>
2022-08-24 14:18:39 -0300

    Adicionando Ex02 e modificando Ex01

--- Ex02.py
+++ Ex02.py
@@ -2,6 +2,6 @@ n1 = int(input("Digite um número: "))
 n2 = int(input("Digite outro número: "))
 
 if n1 < n2:
-    print(n2, "é o maior número!")
+    print(n1, "é o menor número!")
 elif n2 < n1:
-    print(n1, "é o maior número!")
+    print(n2, "é o menor número!")
