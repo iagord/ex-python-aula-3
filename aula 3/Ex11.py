8ee7593449fd8526f45ad4cc37cd5ab72b9e8757
iagord <rosadiasiago@gmail.com>
2022-08-30 21:37:08 -0300

    modificação-Ex11

--- aula 3/Ex11.py
+++ aula 3/Ex11.py
@@ -4,4 +4,13 @@ t = float(input("Digite o tempo de percurso: "))
 
 v = (v0 + (a * t)) * 3.6
 
-print("Velocidade final é", v)
+if (v <= 40):
+    print("Veículo muit lento.")
+elif (v > 40) and (v <= 60):
+    print("Velocidade permitida.")
+elif (v > 60) and (v <= 80):
+    print("Velocidade de cruzeiro.")
+elif (v > 80) and (v <= 120):
+    print("Veícuo rápido.")
+else:
+    print("Veículo muito rápido.")
