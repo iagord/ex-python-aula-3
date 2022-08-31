6899eb8b165607fdbd5ac4c271f3939b36212bc1
iagord <rosadiasiago@gmail.com>
2022-08-30 21:02:21 -0300

    Ex11

---
+++ aula 3/Ex11.py
@@ -0,0 +1,7 @@
+a = float(input("Digite a acelerção: "))
+v0 = float(input("Digite a velocidade inicial: "))
+t = float(input("Digite o tempo de percurso: "))
+
+v = (v0 + (a * t)) * 3.6
+
+print("Velocidade final é", v)
