fc1d6fe610350c9be3159062b5c26399f968c6d0
Logon Aluno <logonrmlocal@fiap.com.br>
2022-09-06 20:46:01 -0300

    Ex34 While

---
+++ aula 3/Ex34W.py
@@ -0,0 +1,7 @@
+print("Tabuada do 5")
+tabuada = 5
+multiplicador = 1
+
+while (multiplicador > 0) and (multiplicador < 11):
+    print(tabuada * multiplicador)
+    multiplicador = multiplicador + 1
