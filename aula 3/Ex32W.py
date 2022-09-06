5c08a5c0d77022368a17478ef03eb0139d9ba0d7
Logon Aluno <logonrmlocal@fiap.com.br>
2022-09-06 20:18:16 -0300

    Ex32 While

---
+++ aula 3/Ex32W.py
@@ -0,0 +1,6 @@
+v1 = float(input("Digite o primero valor: "))
+v2 = float(input("Digite o segundo valor: "))
+
+while (v1 > v2):
+    print("O segundo valor deve ser maior que o primeiro.")
+    v2 = float(input("Digite o segundo valor novamente: "))
