3a502ec2f072d5caadcd926623fcb363293c61a4
Logon Aluno <logonrmlocal@fiap.com.br>
2022-09-06 20:58:10 -0300

    Ex35 For

--- aula 3/Ex34W.py
+++ aula 3/Ex34W.py
@@ -3,5 +3,6 @@ tabuada = 5
 multiplicador = 1
 
 while (multiplicador > 0) and (multiplicador < 11):
+    multiplicador = 1
     print(tabuada * multiplicador)
     multiplicador = multiplicador + 1
