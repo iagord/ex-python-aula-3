dc29146dd0918bcc669fe6cb3d84464346b231ce
iagord <rosadiasiago@gmail.com>
2022-09-13 20:29:03 -0300

    Adicionando Exercícios

--- aula 3/Ex50W.py
+++ aula 3/Ex50W.py
@@ -1,5 +1,3 @@
-#Elabore um programa que apresente os números pares maiores que 10 no intervalo fechado [A, B]. Sendo que A e B serão números inteiros escolhidos pelo usuário. Um número é par quando este satisfaz a seguinte condição: (NÚMERO mod 2 = 0)
-
 A = int(input("Digite o número inicial: "))
 B = int(input("Digite o número final: "))
 n = A
