a84d06b0c1d566cbbf1e62aed82dc754b6586707
iagord <rosadiasiago@gmail.com>
2022-09-13 17:08:44 -0300

    Adcicionando exercícios

---
+++ aula 3/Ex44.py
@@ -0,0 +1,3 @@
+# Entrar via teclado com dez valores positivos. Consistir a digitação e enviar mensagem de erro, se necessário. Após a digitação, exibir: O maior valor, A soma dos valores, A média aritmética dos valores;
+
+
